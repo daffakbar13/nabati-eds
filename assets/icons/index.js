c599ffaf43034e78f892ed1fbba9b856634ee775
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-10-12 22:53:54 +0700

    Add Assets

---
+++ assets/icons/index.js
@@ -0,0 +1,55 @@
+import ICField from './field-list.svg'
+import ICUser from './user-list.svg'
+import ICMenu from './Ic_menu.svg'
+import ICCalendar from './calendar.svg'
+import ICFinance from './finance-group-logo.svg'
+import ICExclamation from './exclamation.svg'
+import ICDownload from './ic-download.svg'
+import ICUpload from './ic-upload.svg'
+import ICSync from './ic-sync.svg'
+import ICEyeCrossed from './eye-crossed.svg'
+import ICInventory from './ic-inventory.svg'
+import ICPurchaseOrg from './ic-purchase-org.svg'
+import ICPlus from './ic-plus.svg'
+import ICEdit from './ic-edit.svg'
+import ICDelete from './ic-delete.svg'
+import ICPlusWhite from './ic-plus-white.svg'
+import ICCheckPrimary from './check-as-primary.svg'
+import ICView from './ic-view.svg'
+import ICDollar from './dollar.svg'
+import ICManageCustGroupBuyingPrice from './manage-cust-group-buying-price.svg'
+import IconAdd from './ICAdd'
+import ICDollarBlack from './ic-dollar-black.svg'
+import ICInfo from './ic-info.svg'
+import ICCopy from './ic-copy.svg'
+import ICPackage from './ic-package.svg'
+import ICArrowRight from './ic-arrow-right.svg'
+
+export {
+  IconAdd,
+  ICField,
+  ICUser,
+  ICMenu,
+  ICCalendar,
+  ICFinance,
+  ICExclamation,
+  ICDownload,
+  ICUpload,
+  ICSync,
+  ICEyeCrossed,
+  ICInventory,
+  ICPurchaseOrg,
+  ICPlus,
+  ICEdit,
+  ICDelete,
+  ICPlusWhite,
+  ICCheckPrimary,
+  ICView,
+  ICDollar,
+  ICManageCustGroupBuyingPrice,
+  ICDollarBlack,
+  ICInfo,
+  ICCopy,
+  ICPackage,
+  ICArrowRight,
+}
