d5819586d52f320046dac258c0f9ea65693d3480
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-10-12 21:12:59 +0700

    initiate boilerplate

---
+++ pages/api/login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Sidebar,
+  Layout,
+  Header,
+  MenuLogout,
+  Notification,
+  Spacer,
+} from "pink-lava-ui";
+
+export default function Login() {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <div>Login</div>;
+    </Layout>
+  );
+}
---
+++ pages/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Sidebar,
+  Layout,
+  Header,
+  MenuLogout,
+  Notification,
+  Spacer,
+  Alert,
+  Text,
+} from "pink-lava-ui";
+
+export default function Login() {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Text varian="headingMedium">Test</Text>
+      <Alert>Test alert</Alert>
+    </Layout>
+  );
+}
