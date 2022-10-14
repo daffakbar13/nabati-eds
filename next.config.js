7f0bfb913b8f07b34b0b80d04c7bd4a0e2bd942c
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-10-14 11:16:56 +0700

    set up next config

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //   images: {
+  //     domains: ['mdm-portal.nabatisnack.co.id'],
+  //   },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
