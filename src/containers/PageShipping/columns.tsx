f381ea300c3e3dbb4deaee7df13615600164b42d
r_fakhri <fakhri.rafiki@gmail.com>
2022-10-18 00:59:56 +0700

    Add Billing Page

---
+++ src/containers/PageShipping/columns.tsx
@@ -0,0 +1,51 @@
+export const columns = [
+  {
+    title: 'Billing Number',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Billing',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Order Type',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Order Date',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Sales Org',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Branch',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Ship to customer',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Shipment Number',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+  {
+    title: 'Salesman',
+    dataIndex: 'billingNumber',
+    key: 'billingNumber',
+  },
+]
+
+export default {
+  columns,
+}
---
+++ src/containers/PageShipping/styledComponents.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Example = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+export default {
+  Example,
+}
