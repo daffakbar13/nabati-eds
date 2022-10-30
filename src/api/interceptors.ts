cc5dba35eeea28eae2913ff5b7ab96873b1d37fd
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-10-30 21:46:11 +0700

    interceptor

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -2,7 +2,17 @@ import { AxiosError, AxiosResponse } from 'axios'
 import Router from 'next/router'
 import { toCamelCase } from 'src/utils/formatter'
 import { PUBLIC_URL } from 'src/configs/env'
-import MSG from './http-error-message'
+import {
+  HTTP_ERROR,
+  HTTP_UNAUTHORIZED_ACTION,
+  HTTP_INTERNAL_SERVER_ERROR,
+  HTTP_LOGIN_ERROR,
+  HTTP_UNKNOWN_ERROR,
+  HTTP_UNPROCESSED_REQUEST,
+  HTTP_NETWORK_ERROR,
+  HTTP_NETWORK_ISSUE_ERROR,
+  HTTP_SESSION_EXPIRED,
+} from './http-error-message'
 
 const LOGIN_SUB_URL = 'login'
 
@@ -16,25 +26,25 @@ export const errorInterceptor = (err: AxiosError): Promise<never> => {
   if (response) {
     const url: string = response?.config?.url || ''
     if (response.status === 401 && url.includes(LOGIN_SUB_URL)) {
-      alert(MSG.HTTP_LOGIN_ERROR)
+      alert(HTTP_LOGIN_ERROR)
       localStorage.clear()
     } else if (response.status === 401) {
-      alert(MSG.HTTP_SESSION_EXPIRED)
+      alert(HTTP_SESSION_EXPIRED)
       window.setTimeout(() => {
         localStorage.clear()
         window.location.href = `${PUBLIC_URL}/login`
       }, 1500)
     } else if (response.status === 403) {
-      alert(MSG.HTTP_UNAUTHORIZED_ACTION)
+      alert(HTTP_UNAUTHORIZED_ACTION)
     } else if (response.status === 500) {
-      alert(MSG.HTTP_INTERNAL_SERVER_ERROR)
+      alert(HTTP_INTERNAL_SERVER_ERROR)
     } else if (response.status === 503) {
       Router.push('/maintenance')
     } else {
-      alert(MSG.HTTP_NETWORK_ISSUE_ERROR)
+      alert(HTTP_NETWORK_ISSUE_ERROR)
     }
   } else {
-    alert(MSG.HTTP_NETWORK_ERROR)
+    alert(HTTP_NETWORK_ERROR)
   }
   return Promise.reject(err)
 }
