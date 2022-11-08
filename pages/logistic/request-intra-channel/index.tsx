49f8468ee4d9bdf3a15984ff21644cbf6184deb5
muhammad taufik zamaludin <taufikmuhammad550@gmail.com>
2022-11-08 16:34:08 +0700

    implement ui request intra channel

---
+++ pages/logistic/request-intra-channel/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageIntraChannelRequest from 'src/containers/PageIntraChannelRequest'
+
+interface IntraChannelRequestProps { }
+
+export default function Quotation(props: IntraChannelRequestProps) {
+    const { } = props
+
+    return <PageIntraChannelRequest />
+}
