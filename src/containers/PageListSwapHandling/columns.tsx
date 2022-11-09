61f022657f2d05d115d2d28bd466f11ece8a7945
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-11-09 09:52:07 +0700

    List Swap Handling

--- src/containers/PageListSwapHandling/columns.tsx
+++ src/containers/PageListSwapHandling/columns.tsx
@@ -1,52 +1,74 @@
 import CreateColumns, { dataIndexWithSorter } from 'src/utils/createColumns'
+import moment from 'moment'
 // import { useRouter } from 'next/router'
 // import React from 'react'
 // import { Button } from 'pink-lava-ui'
 // import { PATH } from 'src/configs/menus'
 
 export const columns = [
-  {
-    title: 'Material Document',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Movement Type',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'From',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'To',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Material Date',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'No. Reference',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Material',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Large',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Middle',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'Small',
-    ...dataIndexWithSorter('branch_id'),
-  },
-  {
-    title: 'No. Ref. Cancel',
-    ...dataIndexWithSorter('branch_id'),
-  },
+  CreateColumns(
+    'Material Document',
+    'material_document',
+    true,
+    (text) => <>{text}</>,
+    250,
+  ),
+  CreateColumns(
+    'Movement Type',
+    'movement_type_name',
+    true,
+    (text) => <>{text}</>,
+    200,
+  ),
+  CreateColumns(
+    'From',
+    'from_sloc',
+    true,
+    (text) => <>{text}</>,
+    100,
+  ),
+  CreateColumns(
+    'To',
+    'to_sloc',
+    true,
+    (text) => <>{text}</>,
+    100,
+  ),
+  CreateColumns(
+    'Material Date',
+    'material_date',
+    true,
+    (text) => <>{moment(text).format('DD-MM-YYYY')}</>,
+    200,
+  ),
+  CreateColumns(
+    'No. Reference',
+    'no_reference',
+    true,
+  ),
+  CreateColumns(
+    'Material',
+    'material_document',
+    true,
+  ),
+  CreateColumns(
+    'Large',
+    'large',
+    true,
+  ),
+  CreateColumns(
+    'Middle',
+    'middle',
+    true,
+  ),
+  CreateColumns(
+    'Small',
+    'small',
+    true,
+  ),
+  CreateColumns(
+    'No. Ref. Cancel',
+    'no_reference_cancel',
+    true,
+  ),
 ]
