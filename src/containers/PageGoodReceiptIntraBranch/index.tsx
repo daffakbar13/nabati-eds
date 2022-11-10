3fade7aa27e5a28e3fbb96aa2e963af8cc81fcd3
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-11-10 23:53:38 +0700

    Good Issue List

--- src/containers/PageGoodReceiptIntraBranch/index.tsx
+++ src/containers/PageGoodReceiptIntraBranch/index.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
-import { MoreOutlined } from '@ant-design/icons'
-import { Pagination, Checkbox, Popover, Divider, Typography } from 'antd'
+import React, { } from 'react'
+import { useRouter } from 'next/router'
 import { Button, Col, Row, Search, Spacer, Text, Table } from 'pink-lava-ui'
-
-import { Card, FloatAction, Popup } from 'src/components'
-import { useTable, useTitlePage } from 'src/hooks'
+import { Card } from 'src/components'
 import { colors } from 'src/configs/colors'
-
-import { getStockRealtimeList } from 'src/api/logistic/stock-real-time'
-
+import { Pagination, Checkbox, Popover, Divider, Typography } from 'antd'
+import useTable from 'src/hooks/useTable'
+import { MoreOutlined } from '@ant-design/icons'
+import useTitlePage from 'src/hooks/useTitlePage'
+import FloatAction from 'src/components/FloatAction'
+import { getQuotation } from 'src/api/quotation'
+import Popup from 'src/components/Popup'
 import SmartFilter, { FILTER, useSmartFilters } from 'src/components/SmartFilter'
-import { PageRealTimeProps } from './types'
+import { Props } from './types'
 import { columns } from './columns'
 
 function showTotal(total: number, range: number[]) {
   const ranges = range.join('-')
+  console.log(total, range)
+
   const text = ['Showing', ranges, 'of', total, 'items'].join(' ')
   return <p>{text}</p>
 }
 
-export default function PageGoodReceiptIntraBranch(props: PageRealTimeProps) {
+export default function PageGoodsIssue(props: Props) {
   const { filters, setFilters } = useSmartFilters([
     FILTER.SALES_ORG,
     FILTER.BRANCH,
@@ -28,79 +31,56 @@ export default function PageGoodReceiptIntraBranch(props: PageRealTimeProps) {
     FILTER.ORDER_TYPE,
     FILTER.ORDER_DATE,
   ])
-
   const table = useTable({
-    api: '',
-    funcApi: getStockRealtimeList,
+    funcApi: getQuotation,
     haveCheckbox: { headCell: 'status_name', member: ['New'] },
     columns,
   })
   const titlePage = useTitlePage('list')
   const [showConfirm, setShowConfirm] = React.useState('')
+  const hasData = table.total > 0
+  const router = useRouter()
+  const oneSelected = table.selected.length === 1
+  const firstSelected = table.selected[0]
 
-  const content = (
-    <>
-      {columns.map(({ title }, index) => (
-        <div key={index}>
-          <Checkbox
-            defaultChecked={!table.hiddenColumns.includes(title)}
-            onChange={(event) => {
-              table.handleHideShowColumns(event.target, title)
-            }}
-          />{' '}
-          {title}
-        </div>
-      ))}
-      <Divider />
-      <h4
-        onClick={table.handleResetHideShowColumns}
-        style={{ textAlign: 'center', cursor: 'pointer' }}
-      >
-        Reset
-      </h4>
-    </>
-  )
+  const selectedQuotation = {
+    text: oneSelected ? firstSelected : `${firstSelected}, More +${table.selected.length - 1}`,
+    content: (
+      <div style={{ textAlign: 'center' }}>
+        {table.selected.join(', ')}
+      </div>
+    ),
+  }
 
-  const HideShowColumns = () => (
-    <Popover placement="bottomRight" title={'Hide/Show Columns'} content={content} trigger="click">
-      <span style={{ color: '#f0f0f0' }}>___</span>
-      <MoreOutlined />
-      <span style={{ color: '#f0f0f0' }}>___</span>
-    </Popover>
-  )
-
-  // useEffect(() => {
-  //     const fetchData = () => {
-  //         const myHeaders = new Headers();
-  //         myHeaders.append('Content-Type', 'application/json');
-
-  //         const raw = JSON.stringify({
-  //             filters: [
-  //                 {
-  //                     field: 'product_id',
-  //                     option: 'BT',
-  //                     from_value: '300006',
-  //                     to_value: '300007',
-  //                     data_type: 'S',
-  //                 },
-  //             ],
-  //             limit: 8,
-  //             page: 1,
-  //         });
-
-  //         fetch('https://dist-system.nabatisnack.co.id:3002/v1/stocks/list', {
-  //             method: 'POST',
-  //             headers: myHeaders,
-  //             body: raw,
-  //             redirect: 'follow',
-  //         })
-  //             .then((response) => response.text())
-  //             .then((result) => console.log(result))
-  //             .catch((error) => console.log('error', error));
-  //     }
-
-  //     fetchData()
-  // }, [])
+  const HideShowColumns = () => {
+    const content = (
+      <>
+        {columns.map(({ title }, index) => (
+          <div key={index}>
+            <Checkbox
+              defaultChecked={!table.hiddenColumns.includes(title)}
+              onChange={(event) => {
+                table.handleHideShowColumns(event.target, title)
+              }}
+            />{' '}
+            {title}
+          </div>
+        ))}
+        <Divider />
+        <h4
+          onClick={table.handleResetHideShowColumns}
+          style={{ textAlign: 'center', cursor: 'pointer', color: '#EB008B' }}
+        >
+          Reset
+        </h4>
+      </>
+    )
+    return (
+      <Popover placement="bottomRight" title={'Hide/Show Columns'} content={content} trigger="click">
+        <MoreOutlined style={{ cursor: 'pointer' }} />
+      </Popover>
+    )
+  }
 
   return (
     <Col>
@@ -116,34 +96,62 @@ export default function PageGoodReceiptIntraBranch(props: PageRealTimeProps) {
               colorIcon={colors.grey.regular}
               onChange={() => { }}
             />
-            <SmartFilter onOk={setFilters} filters={filters} />
+            <SmartFilter
+              onOk={(newVal) => {
+                const newFiltered = newVal
+                  .filter((obj) => obj.fromValue)
+                  .map((obj) => ({
+                    field: `eds_order.${obj.field}`,
+                    option: obj.option,
+                    from_value: obj.fromValue.value,
+                    to_value: obj.toValue?.value,
+                  }))
+                setFilters(newVal)
+                table.handleFilter(newFiltered)
+                // setFiltered(newFiltered)
+                console.log('newVal', newVal)
+              }}
+              filters={filters} />
           </Row>
-          {/* <Row gap="16px">
-                        <Button size="big" variant="secondary" onClick={() => { }}>
-                            Download
-                        </Button>
-                    </Row> */}
+          <Row gap="16px">
+            <Button size="big" variant="secondary" onClick={() => { }}>
+              Download
+            </Button>
+            <Button
+              size="big"
+              variant="primary"
+              onClick={() => router.push(`${router.pathname}/create`)}
+            >
+              Create
+            </Button>
+          </Row>
         </Row>
       </Card>
       <Spacer size={10} />
       <Card style={{ padding: '16px 20px' }}>
-        <Table
-          loading={table.loading}
-          columns={[...table.columns, { title: <HideShowColumns />, width: 50 }]}
-          dataSource={table.data}
-          showSorterTooltip={false}
-          pagination={false}
-        />
-        <Pagination
-          defaultPageSize={20}
-          pageSizeOptions={[20, 50, 100]}
-          showLessItems
-          showSizeChanger
-          showQuickJumper
-          responsive
-          total={table.data.length}
-          showTotal={showTotal}
-        />
+        <div style={{ display: 'flex', flexGrow: 1, overflow: 'scroll' }}>
+          <Table
+            loading={table.loading}
+            columns={[...table.columns, { title: <HideShowColumns />, width: 50 }]}
+            dataSource={table.data}
+            showSorterTooltip={false}
+            rowSelection={table.rowSelection}
+            rowKey={'id'}
+          />
+        </div>
+        {hasData && (
+          <Pagination
+            defaultPageSize={20}
+            pageSizeOptions={[20, 50, 100]}
+            showLessItems
+            showSizeChanger
+            showQuickJumper
+            responsive
+            total={table.total}
+            showTotal={showTotal}
+            onChange={(page, limit) => { table.handlePagination(page, limit) }}
+          />
+        )}
         {table.selected.length > 0 && (
           <FloatAction>
             <div
@@ -157,7 +165,7 @@ export default function PageGoodReceiptIntraBranch(props: PageRealTimeProps) {
             </div>
             <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'end', gap: 10 }}>
               <Button size="big" variant="tertiary" onClick={() => { }}>
-                Cancel
+                Cancel Process
               </Button>
               <Button
                 size="big"
@@ -172,19 +180,25 @@ export default function PageGoodReceiptIntraBranch(props: PageRealTimeProps) {
           </FloatAction>
         )}
         {showConfirm === 'submit' && (
-          <Popup>
+          <Popup onOutsideClick={() => { setShowConfirm('') }}>
             <Typography.Title level={3} style={{ margin: 0 }}>
               Confirm Submit
             </Typography.Title>
             <Typography.Title level={5} style={{ margin: 0 }}>
-              Are you sure to submit quotation {table.selected.join(', ')} ?
+              Are you sure to submit quotation
+              {oneSelected
+                ? ` ${selectedQuotation.text} ?`
+                : <Popover content={selectedQuotation.content}>
+                  {` ${selectedQuotation.text} ?`}
+                </Popover>
+              }
             </Typography.Title>
-            <div>
-              <Button size="big" variant="secondary" onClick={() => { }}>
-                Download
+            <div style={{ display: 'flex', gap: 10 }}>
+              <Button size="big" style={{ flexGrow: 1 }} variant="secondary" onClick={() => { router.reload() }}>
+                Cancel Proccess
               </Button>
-              <Button size="big" variant="primary" onClick={() => { }}>
-                Create
+              <Button size="big" style={{ flexGrow: 1 }} variant="primary" onClick={() => { router.reload() }}>
+                Submit
               </Button>
             </div>
           </Popup>
