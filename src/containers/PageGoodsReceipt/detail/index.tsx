e94ff8768b8e58b1ca0a0b34b59be0ef1f4f565a
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-11-21 01:26:14 +0700

    Add Print Button

--- src/containers/PageGoodsReceipt/detail/index.tsx
+++ src/containers/PageGoodsReceipt/detail/index.tsx
@@ -7,64 +7,67 @@ import { getGoodReceiptDetail } from 'src/api/logistic/good-receipt'
 import { PATH } from 'src/configs/menus'
 
 import DocumentHeader from './Tabs/DocumentHeader'
-import Lpb from './Tabs/Lpb'
+import Lpb from './Tabs/LPB'
 
 export default function DetailGR() {
-    const [loading, setLoading] = useState(false)
-    const [details, setDetails] = useState<{ items: [] }>({ items: [] })
-    const router = useRouter()
-    const id = String(router.query.id) || ''
+  const [loading, setLoading] = useState(false)
+  const [details, setDetails] = useState<{ items: [] }>({ items: [] })
+  const router = useRouter()
+  const id = String(router.query.id) || ''
 
-    useEffect(() => {
-        if (!id) return
-        const fetchData = async () => {
-            try {
-                setLoading(true)
-                const res = await getGoodReceiptDetail(id)
-                setDetails(res.data)
-                setLoading(false)
-            } catch (error) {
-                setLoading(false)
-                console.error(error)
-            }
-        }
-        fetchData()
-    }, [id])
+  const hashTab = router.asPath.split('#')[1]
 
-    console.log('details', details.items);
+  useEffect(() => {
+    if (!id) return
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const res = await getGoodReceiptDetail(id)
+        setDetails(res.data)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [id])
 
-    return (
-        <Col>
-            <div style={{ display: 'flex', gap: 5 }}>
-                <GoBackArrow to={`${PATH.LOGISTIC}/goods-receipt`} />
-                <Text variant={'h4'}>View GR From Principal {`${router.query.id}`}</Text>
-                <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'end', gap: 10 }}>
-                    <Button
-                        size="big"
-                        variant="tertiary"
-                        onClick={() => { }}
-                        loading={loading}
-                    >
-                        Cancel Process
-                    </Button>
-                </div>
-            </div>
-            <Spacer size={20} />
-            <Card style={{ padding: 0 }}>
-                <Tabs items={[
-                    {
-                        key: '1',
-                        tab: 'Document Header',
-                        children: <DocumentHeader loading={loading} details={details} />,
-                    },
-                    {
-                        key: '2',
-                        tab: 'LPB',
-                        children: <Lpb details={details} />,
-                    },
-                ]
-                } />
-            </Card>
-        </Col>
-    )
+  return (
+    <Col>
+      <div style={{ display: 'flex', gap: 5 }}>
+        <GoBackArrow to={`${PATH.LOGISTIC}/goods-receipt`} />
+        <Text variant={'h4'}>View GR From Principal {`${router.query.id}`}</Text>
+        <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'end', gap: 10 }}>
+          {hashTab === '1' && (
+            <Button size="big" variant="tertiary" onClick={() => {}} loading={loading}>
+              Cancel Process
+            </Button>
+          )}
+          {hashTab === '2' && (
+            <Button size="big" variant="primary" onClick={() => {}} loading={loading}>
+              Print LPB
+            </Button>
+          )}
+        </div>
+      </div>
+      <Spacer size={20} />
+      <Card style={{ padding: 0 }}>
+        <Tabs
+          items={[
+            {
+              key: '1',
+              tab: 'Document Header',
+              children: <DocumentHeader loading={loading} details={details} />,
+            },
+            {
+              key: '2',
+              tab: 'LPB',
+              children: <Lpb details={details} />,
+            },
+          ]}
+        />
+      </Card>
+    </Col>
+  )
 }
--- src/containers/PageGrReturn/detail/index.tsx
+++ src/containers/PageGrReturn/detail/index.tsx
@@ -7,64 +7,67 @@ import { getGrReturnDetail } from 'src/api/logistic/good-return'
 import { PATH } from 'src/configs/menus'
 
 import DocumentHeader from './Tabs/DocumentHeader'
-import Lrb from './Tabs/Lrb'
+import Lrb from './Tabs/LRB'
 
 export default function DetailGrReturn() {
-    const [loading, setLoading] = useState(false)
-    const [details, setDetails] = useState<{ items: [] }>({ items: [] })
-    const router = useRouter()
-    const id = String(router.query.id) || ''
+  const [loading, setLoading] = useState(false)
+  const [details, setDetails] = useState<{ items: [] }>({ items: [] })
+  const router = useRouter()
+  const id = String(router.query.id) || ''
 
-    useEffect(() => {
-        if (!id) return
-        const fetchData = async () => {
-            try {
-                setLoading(true)
-                const res = await getGrReturnDetail(id)
-                setDetails(res.data)
-                setLoading(false)
-            } catch (error) {
-                setLoading(false)
-                console.error(error)
-            }
-        }
-        fetchData()
-    }, [id])
+  const hashTab = router.asPath.split('#')[1]
 
-    console.log('details', details.items);
+  useEffect(() => {
+    if (!id) return
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const res = await getGrReturnDetail(id)
+        setDetails(res.data)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [id])
 
-    return (
-        <Col>
-            <div style={{ display: 'flex', gap: 5 }}>
-                <GoBackArrow to={`${PATH.LOGISTIC}/gr-return`} />
-                <Text variant={'h4'}>View GR Return From Principal {`${router.query.id}`}</Text>
-                <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'end', gap: 10 }}>
-                    <Button
-                        size="big"
-                        variant="tertiary"
-                        onClick={() => { }}
-                        loading={loading}
-                    >
-                        Cancel Process
-                    </Button>
-                </div>
-            </div>
-            <Spacer size={20} />
-            <Card style={{ padding: 0 }}>
-                <Tabs items={[
-                    {
-                        key: '1',
-                        tab: 'Document Header',
-                        children: <DocumentHeader loading={loading} details={details} />,
-                    },
-                    {
-                        key: '2',
-                        tab: 'LRB',
-                        children: <Lrb details={details} />,
-                    },
-                ]
-                } />
-            </Card>
-        </Col>
-    )
+  return (
+    <Col>
+      <div style={{ display: 'flex', gap: 5 }}>
+        <GoBackArrow to={`${PATH.LOGISTIC}/gr-return`} />
+        <Text variant={'h4'}>View GR Return From Principal {`${router.query.id}`}</Text>
+        <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'end', gap: 10 }}>
+          {hashTab === '1' && (
+            <Button size="big" variant="tertiary" onClick={() => {}} loading={loading}>
+              Cancel Process
+            </Button>
+          )}
+          {hashTab === '2' && (
+            <Button size="big" variant="primary" onClick={() => {}} loading={loading}>
+              Print LRB
+            </Button>
+          )}
+        </div>
+      </div>
+      <Spacer size={20} />
+      <Card style={{ padding: 0 }}>
+        <Tabs
+          items={[
+            {
+              key: '1',
+              tab: 'Document Header',
+              children: <DocumentHeader loading={loading} details={details} />,
+            },
+            {
+              key: '2',
+              tab: 'LRB',
+              children: <Lrb details={details} />,
+            },
+          ]}
+        />
+      </Card>
+    </Col>
+  )
 }
