812b5ce7d66a6ddd3e79fcc62e5a7eb10066186f
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-11-22 10:53:49 +0700

    add idr in total component

--- src/containers/PageShipment/create/index.tsx
+++ src/containers/PageShipment/create/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import moment from 'moment'
-import { Divider, Table } from 'antd'
+import { Divider, Popover, Table } from 'antd'
 import { Button, Col, Row, Search, Spacer, Text, DatePickerInput } from 'pink-lava-ui'
 import { ICPlusWhite } from 'src/assets/icons'
 import DebounceSelect from 'src/components/DebounceSelect'
@@ -8,6 +8,9 @@ import { Card, TableEditable } from 'src/components'
 import useTitlePage from 'src/hooks/useTitlePage'
 import { fakeApi } from 'src/api/fakeApi'
 import { CommonSelectValue, antdColumns } from 'src/configs/commonTypes'
+import { useRouter } from 'next/router'
+import { colors } from 'src/configs/colors'
+import { DownOutlined } from '@ant-design/icons'
 import { columns } from './columns'
 
 interface Item {
@@ -23,27 +26,62 @@ const originData: Item[] = []
 
 export default function CreateSalesOrder() {
   const [data, setData] = useState<Item[]>(originData)
+  const [showFilter, setShowFilter] = React.useState(false)
   const titlePage = useTitlePage('create')
+  const router = useRouter()
 
   return (
     <Col>
       <Text variant={'h4'}>{titlePage}</Text>
       <Spacer size={20} />
       <Card style={{ overflow: 'unset' }}>
-        <Row justifyContent="space-between" reverse>
+        <Row justifyContent="space-between">
           <Row gap="16px">
-            <Button size="big" variant="tertiary" onClick={() => {}}>
-              Cancel
+            <Search
+              width="380px"
+              nameIcon="SearchOutlined"
+              placeholder="Search Menu Design Name"
+              colorIcon={colors.grey.regular}
+              onChange={() => {}}
+            />
+            <Button
+              size="big"
+              variant="tertiary"
+              // onClick={() => router.push(`${router.pathname}/create`)}
+            >
+              Filter
             </Button>
-            <Button size="big" variant="secondary" onClick={() => {}}>
-              Save As Draft
+            {/* <SmartFilter onOk={setFilters} filters={filters} /> */}
+          </Row>
+          <Row gap="16px">
+            <Button
+              size="big"
+              variant="tertiary"
+              onClick={() => router.push(`${router.pathname}/create`)}
+            >
+              Create
             </Button>
-            <Button size="big" variant="primary" onClick={() => {}}>
-              Submit
+            {/* <Popover placement="bottom" content={moreContent} trigger="click"> */}
+            <Button
+              size="big"
+              variant="secondary"
+              // onClick={downloadTemplateQuotation}
+              style={{ gap: 5 }}
+            >
+              More <DownOutlined />
+            </Button>
+            {/* </Popover> */}
+            <Button
+              size="big"
+              variant="primary"
+              onClick={() => router.push(`${router.pathname}/create`)}
+            >
+              Create
             </Button>
           </Row>
         </Row>
       </Card>
+      <Card style={{ ...(showFilter && { display: 'none' }) }}>test</Card>
       <Spacer size={10} />
       <Card style={{ overflow: 'unset', padding: '28px 20px' }}>
         <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 20 }}>
