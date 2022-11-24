14d6a0faf89e59a1c9f0e5622014ef502a8bc91c
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-11-24 08:11:48 +0700

    add react to print

--- src/containers/PageShipment/detail/tabs/BPB.tsx
+++ src/containers/PageShipment/detail/tabs/BPB.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable camelcase */
 import React from 'react'
+import ReactToPrint from 'react-to-print'
 
 interface BPBProps {}
 
 export default function BPB(props: BPBProps) {
   const {} = props
+  const componentRef = React.useRef()
 
   return (
     <>
-      <div onClick={() => window.print()}>asdasdas</div>
+      <ReactToPrint
+        trigger={() => <button>Print this out!</button>}
+        content={() => componentRef.current}
+      />
+      <div ref={componentRef}>asdasd</div>
     </>
   )
 }
