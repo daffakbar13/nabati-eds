cb50e77fcd63e1540b99115bca6522a3df5caa81
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2022-11-25 20:03:12 +0700

    Sebagai admin saya dapat melihat dan mengakses data create BS Reservation
    -layouting create bs
    -get list item product by branch untuk select item
    -get list uom by product untuk select uom
    -consume create bs

---
+++ pages/logistic/gi-disposal/create/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageGIDisposalCreate from 'src/containers/PageGIDisposal/create'
+
+interface props { }
+
+export default function GIDisposalCreate(props: props) {
+    const { } = props
+
+    return <PageGIDisposalCreate />
+}
