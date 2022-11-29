83b66053424f967dc9e05b569577a92aea60098f
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2022-11-29 16:17:25 +0700

    fixing strict usedetail hooks

--- src/hooks/useDetail.ts
+++ src/hooks/useDetail.ts
@@ -6,21 +6,33 @@ import { CommonDetailParams } from 'src/api/types'
 export default function useDetail(
   funcApi?: (params: CommonDetailParams) => Promise<any>,
   params?: CommonDetailParams,
+  strict: boolean = true,
 ) {
   const [data, setData] = React.useState<any>({})
   const router = useRouter()
   const backPage = router.asPath.split('/').splice(0, 3).join('/')
 
   React.useEffect(() => {
-    if (router.query.status === 'Draft' || !router.query.status) {
-      router.push(backPage)
-    } else if (!Object.values(params).includes(undefined)) {
-      funcApi(params)
-        .then((results) => setData(results.data))
-        .catch(() => {
-          router.push(backPage)
-        })
+    if (strict) {
+      if (router.query.status === 'Draft' || !router.query.status) {
+        router.push(backPage)
+      } else if (!Object.values(params).includes(undefined)) {
+        funcApi(params)
+          .then((results) => setData(results.data))
+          .catch(() => {
+            router.push(backPage)
+          })
+      }
+    } else {
+      if (!Object.values(params).includes(undefined)) {
+        funcApi(params)
+          .then((results) => setData(results.data))
+          .catch(() => {
+            router.push(backPage)
+          })
+      }
     }
+
   }, [router.query])
 
   return data
