4704a7d84f4e9c47c0c465875d671ff27dba7699
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-11-30 14:18:17 +0700

    change to createDataList from component DataList

--- src/containers/PageCollection/detail/tabs/Quotation.tsx
+++ src/containers/PageCollection/detail/tabs/Quotation.tsx
@@ -1,34 +1,34 @@
-import { Col, Row, Table, Divider } from 'antd'
+import { Col, Row, Divider } from 'antd'
 import React from 'react'
 import DataList from 'src/components/DataList'
 import Total from 'src/components/Total'
 import useTable from 'src/hooks/useTable'
-import { Spacer } from 'pink-lava-ui'
+import { Spacer, Table } from 'pink-lava-ui'
 import { TableQuotation } from '../columns'
 
 interface QuotationProps {}
 
-const createDataList = (label: string, value: string) => ({ label, value })
+// const DataList.createDataList = (label: string, value: string) => ({ label, value })
 
 export default function Quotation(props: QuotationProps) {
   const {} = props
-  const table = useTable({ api: '', columns: TableQuotation })
+  // const table = useTable({ api: '', columns: TableQuotation })
   const dataList = [
-    createDataList('Quotation', 'ZOP1'),
-    createDataList('Customer', 'ZOP1'),
-    createDataList('Sales Org.', 'ZOP1'),
-    createDataList('Plant', 'ZOP1'),
-    createDataList('Salesman', 'ZOP1'),
-    createDataList('Doc. Date', 'ZOP1'),
-    createDataList('Valid From', 'ZOP1'),
-    createDataList('Valid To', 'ZOP1'),
-    createDataList('Delivery Date', 'ZOP1'),
-    createDataList('Reference', 'ZOP1'),
-    createDataList('Created On', 'ZOP1'),
-    createDataList('Created By', 'ZOP1'),
-    createDataList('Modified On', 'ZOP1'),
-    createDataList('Modified By', 'ZOP1'),
-    createDataList('Created From', 'ZOP1'),
+    DataList.createDataList('Quotation', 'ZOP1'),
+    DataList.createDataList('Customer', 'ZOP1'),
+    DataList.createDataList('Sales Org.', 'ZOP1'),
+    DataList.createDataList('Plant', 'ZOP1'),
+    DataList.createDataList('Salesman', 'ZOP1'),
+    DataList.createDataList('Doc. Date', 'ZOP1'),
+    DataList.createDataList('Valid From', 'ZOP1'),
+    DataList.createDataList('Valid To', 'ZOP1'),
+    DataList.createDataList('Delivery Date', 'ZOP1'),
+    DataList.createDataList('Reference', 'ZOP1'),
+    DataList.createDataList('Created On', 'ZOP1'),
+    DataList.createDataList('Created By', 'ZOP1'),
+    DataList.createDataList('Modified On', 'ZOP1'),
+    DataList.createDataList('Modified By', 'ZOP1'),
+    DataList.createDataList('Created From', 'ZOP1'),
   ]
 
   return (
@@ -52,7 +52,7 @@ export default function Quotation(props: QuotationProps) {
       </Row>
       <Divider />
       <div style={{ overflow: 'scroll' }}>
-        <Table columns={table.columns} dataSource={[]} />
+        <Table columns={TableQuotation} dataSource={[]} />
       </div>
       <Spacer size={30} />
       <Row>
