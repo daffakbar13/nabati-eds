6dd369737bc61b08ad7cb18dfa6b98fd8f660b31
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-02 04:40:15 +0700

    big changes in hooks createColumns

--- src/utils/createColumns.tsx
+++ src/utils/createColumns.tsx
@@ -1,8 +1,10 @@
+import { ColumnType } from 'antd/lib/table'
+import { Table } from 'antd'
 import moment from 'moment'
 import React from 'react'
 
 export default function CreateColumns(
-  title: string | React.ReactNode,
+  title: string,
   dataIndex: string,
   sorter?: boolean,
   render?: (text: string, record?: any, index?: number) => React.ReactNode,
@@ -29,22 +31,75 @@ export default function CreateColumns(
   const isRightAlign = typeof title === 'string' && rightAlign.includes(title)
 
   return {
-    title,
+    title: (
+      <div id={title} style={{ width: 'max-content' }}>
+        {title}
+      </div>
+    ),
     dataIndex,
-    sorter: sorter ? {
-      compare: (a: string, b: string) => {
-        const isDate = moment(a[dataIndex]).isValid() === true
-        if (isDate) return moment(a[dataIndex]).unix() - moment(b[dataIndex]).unix()
-        return a[dataIndex].localeCompare(b[dataIndex])
-      },
-    } : false,
+    sorter: sorter
+      ? {
+          compare: (a: string, b: string) => {
+            const isDate = moment(a[dataIndex]).isValid() === true
+            if (isDate) return moment(a[dataIndex]).unix() - moment(b[dataIndex]).unix()
+            return a[dataIndex].localeCompare(b[dataIndex])
+          },
+        }
+      : false,
     render,
     fixed,
     ellipsis: true,
     width: width || 170,
     className,
     align: isRightAlign ? 'right' : 'left',
-    children: children,
+    children,
+  }
+}
+
+interface addColumnProps extends ColumnType<typeof Table> {
+  render?: (text: string, record?: any, index?: number) => React.ReactNode
+}
+
+export function addColumn(props: addColumnProps) {
+  const { dataIndex, title, sorter } = props
+  const rightAlign = [
+    'Total Amount',
+    'Based Price',
+    'Sub Total',
+    'Gross',
+    'Discount',
+    'Disc 1',
+    'Disc 2',
+    'Disc 3',
+    'Disc 4',
+    'Net 1',
+    'Net 2',
+    'Net 3',
+    'Net 4',
+  ]
+  const isRightAlign = typeof title === 'string' && rightAlign.includes(title)
+  const idx = dataIndex as string
+
+  return {
+    ...props,
+    title: (
+      <div id={title as string} style={{ width: 'max-content' }}>
+        {title}
+      </div>
+    ),
+    sorter: sorter
+      ? {
+          compare: (a: string, b: string) => {
+            const isDate = moment(a[idx]).isValid() === true
+            if (isDate) {
+              return moment(a[idx]).unix() - moment(b[idx]).unix()
+            }
+            return a[idx].localeCompare(b[idx])
+          },
+        }
+      : false,
+    ellipsis: true,
+    align: isRightAlign ? 'right' : 'left',
   }
 }
 
