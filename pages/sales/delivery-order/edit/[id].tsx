b76b236ce4f4260c3ec423b4c179260d06b94b8c
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-01 08:25:59 +0700

    add update page in delivary order

--- pages/sales/delivery-order/edit/[id].tsx
+++ pages/sales/delivery-order/edit/[id].tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageCreateDeliveryOrder from 'src/containers/PageDeliveryOrder/create'
 
 interface DeliveryOrderEditProps {}
 
 export default function DeliveryOrderEdit(props: DeliveryOrderEditProps) {
   const {} = props
 
-  return <div>DeliveryOrderEdit</div>
+  return <PageCreateDeliveryOrder />
 }
