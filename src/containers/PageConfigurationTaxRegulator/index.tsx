4a12a9c831cff9cf700fec89bbe657df921b045d
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-12-08 03:27:39 +0700

    Tax Regulator

---
+++ src/containers/PageConfigurationTaxRegulator/index.tsx
@@ -0,0 +1,101 @@
+import { useRouter } from 'next/router'
+import { Button, Row, Spacer, Table, Text } from 'pink-lava-ui'
+import { useState } from 'react'
+import { Card, SearchQueryParams, Modal } from 'src/components'
+
+import { getConfigSlocCompanyList, updateStatus } from 'src/api/logistic/configuration-sloc-company'
+import { useSimpleTable } from 'src/hooks'
+import { columns } from './columns'
+
+import CreateModal from './create'
+
+export default function PageConfigurationTaxRegulator() {
+  const [filters, setFilters] = useState([])
+  const router = useRouter()
+
+  const [showCreateModal, setShowCreateModal] = useState(false)
+  const [selectedRow, setSelectedRow] = useState(null)
+
+  const goToDetailPage = (row: any) => {
+    setSelectedRow(row)
+    setShowCreateModal(true)
+  }
+
+  const [showChangeStatusModal, setShowChangeStatusModal] = useState(false)
+  const [changeStatusPayload, setChangeStatusPayload] = useState(null)
+  const onClickSwitch = (a: boolean, rec: any) => {
+    setChangeStatusPayload(rec)
+    setShowChangeStatusModal(true)
+  }
+
+  const handleChangeStatus = async () => {
+    const reqBody = { status: changeStatusPayload.status ? 0 : 1 }
+    try {
+      return await updateStatus(reqBody, changeStatusPayload)
+    } catch (error) {
+      console.error(error)
+    }
+    return false
+  }
+
+  const tableProps = useSimpleTable({
+    funcApi: getConfigSlocCompanyList,
+    columns: columns(goToDetailPage, onClickSwitch),
+    filters,
+  })
+
+  console.log('selectedRow', selectedRow)
+
+  return (
+    <>
+      <Text variant={'h4'}>Tax Regulator</Text>
+      <Spacer size={20} />
+      <Card style={{ overflow: 'unset' }}>
+        <Row justifyContent="space-between">
+          <Row gap="16px">
+            <SearchQueryParams />
+          </Row>
+          <Row gap="16px">
+            <Button size="big" variant="primary" onClick={() => setShowCreateModal(true)}>
+              Create
+            </Button>
+          </Row>
+        </Row>
+      </Card>
+      <Spacer size={10} />
+      <Card style={{ padding: '16px 20px', overflow: 'scroll' }}>
+        <div style={{ display: 'flex', flexGrow: 1, overflow: 'scroll' }}>
+          <Table {...tableProps} />
+        </div>
+      </Card>
+
+      <CreateModal
+        visible={showCreateModal}
+        payload={selectedRow || null}
+        close={() => {
+          setSelectedRow(null)
+          setShowCreateModal(false)
+        }}
+      />
+
+      <Modal
+        title={'Confirm Submit'}
+        open={showChangeStatusModal}
+        onOk={handleChangeStatus}
+        onCancel={() => {
+          setShowChangeStatusModal(false)
+        }}
+        content={`Are you sure want to ${
+          changeStatusPayload?.status ? 'inactivate' : 'activate'
+        } this Sloc Company?`}
+        onOkSuccess={() => {
+          router.reload()
+        }}
+        successContent={(res: any) => `Config sloc company has been successfully 
+          ${changeStatusPayload?.status ? 'inactivated' : 'activated'}`}
+        successOkText="OK"
+        width={432}
+      />
+    </>
+  )
+}
