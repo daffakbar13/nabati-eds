9a2d191733555ea59120c5e27e48cbfb8df0aca6
Fakhri Rafiki <fakhrirafiki32@gmail.com>
2022-12-08 03:43:53 +0700

    Branch Location

---
+++ src/containers/PageConfigurationBranchLocation/columns.tsx
@@ -0,0 +1,56 @@
+import moment from 'moment'
+import CreateColumns from 'src/utils/createColumns'
+import { Button, Switch } from 'pink-lava-ui'
+import { Tag } from 'antd'
+import Link from 'src/components/Link'
+
+export const columns = (
+  goToDetail: (rec: any) => void,
+  onClickSwitch: (a: boolean, rec: any) => void,
+) => [
+  CreateColumns('No', '', true, (text: string, rec, index) => <>{index + 1}</>, 70, 'left'),
+  CreateColumns(
+    'Company',
+    'company_name',
+    true,
+    (text, rec) => (
+      <>
+        {text}-{rec.company_name}
+      </>
+    ),
+    200,
+  ),
+  CreateColumns(
+    'Branch From',
+    'company_name',
+    true,
+    (text, rec) => (
+      <>
+        {text}-{rec.company_name}
+      </>
+    ),
+    200,
+  ),
+  CreateColumns(
+    'Branch To',
+    'company_name',
+    true,
+    (text, rec) => (
+      <>
+        {text}-{rec.company_name}
+      </>
+    ),
+    200,
+  ),
+  CreateColumns('Action', 'gr_number', false, (text, rec) => (
+    <Button
+      size="big"
+      variant="tertiary"
+      onClick={() => {
+        goToDetail(rec)
+      }}
+    >
+      View Detail
+    </Button>
+  )),
+]
