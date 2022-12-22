8b24aef13e23fdd4e939085d9645c9f854918882
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2022-12-22 11:31:38 +0700

    fixing stock realtime

--- src/containers/PageStockRealTime/columns.tsx
+++ src/containers/PageStockRealTime/columns.tsx
@@ -1,19 +1,34 @@
 /* eslint-disable */
-import React from 'react'
-import CreateColumns, { addColumn } from 'src/utils/createColumns'
+import { addColumn } from 'src/utils/createColumns'
+// import { useRouter } from 'next/router'
+// import React from 'react'
+// import { Button } from 'pink-lava-ui'
+// import { PATH } from 'src/configs/menus'
+import { Tag, Row, Col } from 'antd'
+import ControlledExpandIcon from 'src/components/ControlledExpandIcon'
 
 export const columns = [
   addColumn({
     title: 'Branch',
     dataIndex: 'branch_id',
     fixed: true,
+    width: 900,
     render: (text, rec) => (
-      <>
-        {text}-{rec.branch_name}
-      </>
+      <Row>
+        <Col span={4}>
+          <ControlledExpandIcon
+            expanded={true}
+            onChange={(expanded) => {
+              console.log('expanded', expanded)
+            }}
+          />
+        </Col>
+        <Col span={20}>
+          {text}-{rec.branch_name}
+        </Col>
+      </Row>
     ),
   }),
-  // CreateColumns('Sloc', 'sloc_id', true, (text) => <>{text}</>, 400),
   addColumn({
     title: 'SLoc',
     dataIndex: 'group_by_sloc',
@@ -36,93 +51,80 @@ export const columns = [
       arr.map((a, ind) => {
         const isLast = arr.length === ind + 1
         return (
-          <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-            {a.product_id} - {a.product_name}
+          <p key={a.group_by_product.product_id} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.product_id} - {a.group_by_product.product_name}
           </p>
         )
       }),
   }),
-  // CreateColumns(
-  //   'Material',
-  //   'product_id',
-  //   true,
-  //   (productId, record) => <>{`${productId} - ${record.product_name}`}</>,
-  //   400,
-  // ),
   addColumn({
-    title: 'Stock',
-    children: [
-      addColumn({
-        title: 'Large',
-        dataIndex: 'group_by_sloc',
-        render: (arr: any[] = []) => {
-          console.log('arr', arr)
-          return arr.map((a, ind) => {
-            const isLast = arr?.length === ind + 1
-            return (
-              <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-                {a.large || '-'}
-              </p>
-            )
-          })
-        },
+    title: 'Large',
+    dataIndex: 'group_by_sloc',
+    fixed: true,
+    render: (arr: any[], record) =>
+      arr.map((a, ind) => {
+        const isLast = arr.length === ind + 1
+        return (
+          <p key={a.group_by_product.large} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.large}
+          </p>
+        )
       }),
-      addColumn({
-        title: 'Middle',
-        dataIndex: 'group_by_sloc',
-        render: (arr: any[] = []) => {
-          return arr.map((a, ind) => {
-            const isLast = arr?.length === ind + 1
-            return (
-              <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-                {a.middle || '-'}
-              </p>
-            )
-          })
-        },
+  }),
+  addColumn({
+    title: 'Middle',
+    dataIndex: 'group_by_sloc',
+    fixed: true,
+    render: (arr: any[], record) =>
+      arr.map((a, ind) => {
+        const isLast = arr.length === ind + 1
+        return (
+          <p key={a.group_by_product.middle} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.middle}
+          </p>
+        )
       }),
-      addColumn({
-        title: 'Small',
-        dataIndex: 'group_by_sloc',
-        render: (arr: any[] = []) => {
-          return arr.map((a, ind) => {
-            const isLast = arr?.length === ind + 1
-            return (
-              <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-                {a.small || '-'}
-              </p>
-            )
-          })
-        },
+  }),
+  addColumn({
+    title: 'Small',
+    dataIndex: 'group_by_sloc',
+    fixed: true,
+    render: (arr: any[], record) =>
+      arr.map((a, ind) => {
+        const isLast = arr.length === ind + 1
+        return (
+          <p key={a.group_by_product.small} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.small}
+          </p>
+        )
       }),
-      addColumn({
-        title: 'Total in large',
-        dataIndex: 'group_by_sloc',
-        render: (arr: any[] = []) => {
-          return arr.map((a, ind) => {
-            const isLast = arr?.length === ind + 1
-            return (
-              <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-                {a.total_in_large || '-'}
-              </p>
-            )
-          })
-        },
+  }),
+  addColumn({
+    title: 'Total in Small',
+    dataIndex: 'group_by_sloc',
+    fixed: true,
+    render: (arr: any[], record) =>
+      arr.map((a, ind) => {
+        const isLast = arr.length === ind + 1
+        return (
+          <p key={a.group_by_product.total_in_small} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.total_in_small}
+          </p>
+        )
       }),
-      addColumn({
-        title: 'Total in small',
-        dataIndex: 'group_by_sloc',
-        render: (arr: any[] = []) => {
-          return arr.map((a, ind) => {
-            const isLast = arr?.length === ind + 1
-            return (
-              <p key={a.sloc_id} style={{ marginBottom: isLast ? 0 : 16 }}>
-                {a.total_in_small || '-'}
-              </p>
-            )
-          })
-        },
+  }),
+  addColumn({
+    title: 'Total in Large',
+    dataIndex: 'group_by_sloc',
+    fixed: true,
+    render: (arr: any[], record) =>
+      arr.map((a, ind) => {
+        const isLast = arr.length === ind + 1
+        return (
+          <p key={a.group_by_product.total_in_large} style={{ marginBottom: isLast ? 0 : 16 }}>
+            {a.group_by_product.total_in_large}
+          </p>
+        )
       }),
-    ],
   }),
 ]
