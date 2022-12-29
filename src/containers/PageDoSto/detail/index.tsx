00d6e57331e94013ad0ba2de38e408b108424442
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2022-12-29 09:20:27 +0700

    fixing update total booking stock

--- src/containers/PageDoSto/detail/index.tsx
+++ src/containers/PageDoSto/detail/index.tsx
@@ -37,7 +37,7 @@ export default function PageDoStoDetail() {
         status_id: '07',
       })
       return await updateTotalBookingStock({
-        branch_id: data.receive_branch_id,
+        branch_id: data.supply_branch_id,
         items: data.items?.map((item: any, index) => {
           return {
             product_id: item.product_id,
