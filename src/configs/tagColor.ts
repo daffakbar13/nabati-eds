ac808f897b7010a0f7c4e3af52ac14a6da54c989
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2023-01-10 16:46:04 +0700

    add Cancelled to red color

--- src/configs/tagColor.ts
+++ src/configs/tagColor.ts
@@ -1,7 +1,7 @@
 export const tagColors = {
   green: ['Done', 'Complete', 'Available', 'Routing Complete', 'Delivery', 'PGI Done'],
   geekblue: ['SAP SO Complete'],
-  red: ['Out of Stock', 'Canceled', 'Cancel', 'Rejected'],
+  red: ['Out of Stock', 'Canceled', 'Cancel', 'Rejected', 'Cancelled'],
   blue: ['Partial', 'Approved', 'SAP SO Complete'],
   orange: ['Open'],
   yellow: ['Draft', 'Wait For Approval', 'Pending', 'Reservation'],
