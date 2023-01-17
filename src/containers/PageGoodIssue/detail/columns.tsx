cd2d75e50648ea8e66594496ed04fb37350e34d6
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2023-01-17 13:58:20 +0700

    fixing column good issue

--- src/containers/PageGoodIssue/detail/columns.tsx
+++ src/containers/PageGoodIssue/detail/columns.tsx
@@ -2,91 +2,76 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-expressions */
 import { Children } from 'react'
-import CreateColumns, { dataIndexWithSorter } from 'src/utils/createColumns'
+import { addColumn } from 'src/utils/createColumns'
 
 export const column = [
-  CreateColumns('No', 'id', false, (text: string, record: any, index: number) => index + 1),
-  CreateColumns(
-    'Item',
-    'product_id',
-    false,
-    (text: string, record: any) => `${record.product_id || ''} - ${record.product_name || ''}`,
-  ),
-  CreateColumns(
-    'Po',
-    'po',
-    false,
-    (text: string, record: any) => `${record.product_id || ''} - ${record.product_name || ''}`,
-    150,
-    false,
-    '',
-    [
-      {
+  addColumn({
+    title: 'No',
+    dataIndex: 'po_number',
+    render: (text, record, index) => index + 1,
+    fixed: true,
+    width: 50,
+  }),
+  addColumn({
+    title: 'Item',
+    dataIndex: 'product_id',
+    render: (text, record, index) => `${text || ''} - ${record.product_name || ''}`,
+  }),
+  addColumn({
+    title: 'PO',
+    dataIndex: 'po',
+    children: [
+      addColumn({
         title: 'Qty',
         dataIndex: 'qty',
-        key: 'qty_po',
-        width: 75,
-      },
-      {
+      }),
+      addColumn({
         title: 'UoM',
         dataIndex: 'uom_id',
-        key: 'uom_po',
-        width: 75,
-      },
+      }),
     ],
-  ),
-  CreateColumns(
-    'Outstanding',
-    'outstanding',
-    false,
-    (text: string, record: any) => `${record.product_id || ''} - ${record.product_name || ''}`,
-    150,
-    false,
-    '',
-    [
-      {
+  }),
+  addColumn({
+    title: 'Outstanding',
+    dataIndex: 'outstanding',
+    children: [
+      addColumn({
         title: 'Qty',
         dataIndex: 'qty',
-        key: 'qty_outstanding',
-        width: 75,
-      },
-      {
+      }),
+      addColumn({
         title: 'UoM',
         dataIndex: 'uom_id',
-        key: 'uom_outstanding',
-        width: 75,
-      },
+      }),
     ],
-  ),
-  CreateColumns(
-    'Received',
-    'received',
-    false,
-    (text: string, record: any) => `${record.product_id || ''} - ${record.product_name || ''}`,
-    150,
-    false,
-    '',
-    [
-      {
+  }),
+  addColumn({
+    title: 'Received',
+    dataIndex: 'received',
+    children: [
+      addColumn({
         title: 'Qty',
         dataIndex: 'qty',
-        key: 'qty_received',
-        width: 75,
-      },
-      {
+      }),
+      addColumn({
         title: 'UoM',
         dataIndex: 'uom_id',
-        key: 'uom_received',
-        width: 75,
-      },
+      }),
     ],
-  ),
-  CreateColumns(
-    'Storage Location',
-    'sloc_id',
-    false,
-    (text: string, record: any) => `${record.sloc_id || ''} - ${record.sloc_name || ''}`,
-  ),
-  CreateColumns('Batch', 'batch', false),
-  CreateColumns('Remarks', 'remarks', false),
+  }),
+  addColumn({
+    title: 'Storage Location',
+    dataIndex: 'sloc_id',
+    render: (text, record, index) => `${text || ''} - ${record.sloc_name || ''}`,
+  }),
+  addColumn({
+    title: 'Batch',
+    dataIndex: 'batch',
+    width: 150,
+  }),
+  addColumn({
+    title: 'Remarks',
+    dataIndex: 'remarks',
+    width: 150,
+  }),
 ]
