a1a9005d7778957ab62d852da1035560bff49316
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-21 15:26:06 +0700

    FEAT: add doc flow in page billing

--- src/containers/PageBilling/detail/tabs/DocumentFlow.tsx
+++ src/containers/PageBilling/detail/tabs/DocumentFlow.tsx
@@ -1,20 +1,9 @@
-import { Table } from 'pink-lava-ui'
+import { useRouter } from 'next/router'
 import React from 'react'
-import useTable from 'src/hooks/useTable'
-import { TableDocumentFlow } from '../columns'
+import { TabDocumentFlow } from 'src/components'
 
-interface DocumentFlowProps {
-  data: any
-}
+export default function DocumentFlow() {
+  const router = useRouter()
 
-export default function DocumentFlow(props: DocumentFlowProps) {
-  const { data } = props
-
-  return (
-    <>
-      <div style={{ overflow: 'scroll' }}>
-        <Table scroll={{ x: 'max-content', y: 600 }} columns={TableDocumentFlow} dataSource={[]} />
-      </div>
-    </>
-  )
+  return <TabDocumentFlow document_id={router.query.id as string} />
 }
