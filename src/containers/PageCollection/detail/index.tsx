542b4c9dfb3776fc058aeeabfb036c29216814b6
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-22 12:03:57 +0700

    FIX: remove unused import in page collection detail

--- src/containers/PageCollection/detail/index.tsx
+++ src/containers/PageCollection/detail/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import { Button as ButtonPinkLava, Col, Spacer, Text } from 'pink-lava-ui'
 import { Card } from 'src/components'
-import { Button, Tabs } from 'antd'
+import { Button } from 'antd'
 import useTitlePage from 'src/hooks/useTitlePage'
-import { PageCollectionDetailProps } from './types'
-import AllTabs from './tabs'
 import Quotation from './tabs/Quotation'
-import DocumentFlow from './tabs/DocumentFlow'
-import CustomerInfo from './tabs/CustomerInfo'
-import SalesmanInfo from './tabs/SalesmanInfo'
 
-export default function PageCollectionDetail(props: PageCollectionDetailProps) {
+export default function PageCollectionDetail() {
   const titlePage = useTitlePage('edit')
-  const [currentTab, setCurrentTab] = React.useState('1')
+  const [currentTab] = React.useState('1')
 
   return (
     <Col>
