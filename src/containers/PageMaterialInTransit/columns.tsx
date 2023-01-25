fb94b55dc4f58d209777ed4825dcc1bc766a6427
muhammad taufik zamaludin <taufik.zamaludin@wgs-id.com>
2023-01-25 22:33:17 +0700

    fixing layouting material in transit

--- src/containers/PageMaterialInTransit/columns.tsx
+++ src/containers/PageMaterialInTransit/columns.tsx
@@ -1,26 +1,56 @@
-import CreateColumns, { dataIndexWithSorter } from 'src/utils/createColumns'
+import { addColumn } from 'src/utils/createColumns'
 // import { useRouter } from 'next/router'
 // import React from 'react'
 // import { Button } from 'pink-lava-ui'
 // import { PATH } from 'src/configs/menus'
 
 export const columns = [
-  CreateColumns('Request Document', 'request_document', true),
-  CreateColumns('Delivery Order Document', 'delivery_order_document', true, (a) => <>{a}</>, 250),
-  CreateColumns('Transaction Type', 'transaction_type', true),
-  CreateColumns('Item', 'item', true),
-  CreateColumns('Material', 'product_id', true, (branchId, record) => (
-    <>{`${record.product_id} - ${record.product_description}`}</>
-  )),
-  CreateColumns('Receiving Branch', 'item', true),
-  CreateColumns(
-    'Supplying Branch',
-    'supplying_branch_id',
-    true,
-    (branchId, record) => <>{`${branchId} - ${record.supplying_branch_name}`}</>,
-    250,
-  ),
-  CreateColumns('Large', 'large', true),
-  CreateColumns('Middle', 'middle', true),
-  CreateColumns('Small', 'small', true),
+  addColumn({
+    title: 'Request Document',
+    dataIndex: 'request_document',
+    fixed: true,
+    sorter: true,
+  }),
+  addColumn({
+    title: 'Delivery Order Document',
+    dataIndex: 'delivery_order_document',
+    fixed: true,
+    sorter: true,
+    width: 250,
+  }),
+  addColumn({
+    title: 'Transaction Type',
+    dataIndex: 'transaction_type',
+  }),
+  addColumn({
+    title: 'Item',
+    dataIndex: 'item',
+  }),
+  addColumn({
+    title: 'Material',
+    dataIndex: 'product_id',
+    render: (text, record, index) => `${text} - ${record.product_description}`,
+  }),
+  addColumn({
+    title: 'Receiving Branch',
+    dataIndex: 'receiving_branch_id',
+    render: (text, record, index) => `${text} - ${record.receiving_branch_name}`,
+  }),
+  addColumn({
+    title: 'Supplying Branch',
+    dataIndex: 'supplying_branch_id',
+    render: (text, record, index) => `${text} - ${record.supplying_branch_name}`,
+  }),
+  addColumn({
+    title: 'Large',
+    dataIndex: 'large',
+  }),
+  addColumn({
+    title: 'Middle',
+    dataIndex: 'middle',
+  }),
+  addColumn({
+    title: 'Small',
+    dataIndex: 'small',
+  }),
 ]
