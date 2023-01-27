ad38fb22f91b48150f42316537f35cb3c5ac9408
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-27 13:38:54 +0700

    FEAT: add new configs numberFormat

--- src/utils/createColumns/configs/numberFormat.ts
+++ src/utils/createColumns/configs/numberFormat.ts
@@ -1,4 +1,4 @@
-const numberFormat = ['Total Amount']
+const numberFormat = ['Total Amount', 'Billing Amount', 'Paid Amount']
 
 export function isNumberFormat(title: unknown) {
   return typeof title === 'string' && numberFormat.includes(title)
