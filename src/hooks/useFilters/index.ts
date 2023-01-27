da278b89e74c33dcb2a408855e8d4cc9c61bdbc9
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-27 11:00:35 +0700

    FEAT: add new config multiple search id with useFilters hooks

--- src/hooks/useFilters/index.ts
+++ src/hooks/useFilters/index.ts
@@ -4,6 +4,9 @@ import { colors } from 'src/configs/colors'
 import { useAppContext } from 'src/contexts'
 import useTable from '../useTable'
 
+type TableType = ReturnType<typeof useTable>
+type FiltersType = TableType['state']['body']['filters']
+
 interface SearchProps {
   placeholder?: string
   width?: string
@@ -19,7 +22,7 @@ export default function useFilters(
   /**
    * Return of Hooks Table
    */
-  table: ReturnType<typeof useTable>,
+  table: TableType,
   /**
    * Placeholder of Search Input
    */
@@ -28,44 +31,45 @@ export default function useFilters(
    * Column Name of ID
    */
   id: string = 'id',
+  /**
+   * Column Name of ID
+   */
+  ids: string[] = ['id'],
 ) {
   const {
     state: { body },
   } = table
   const app = useAppContext()
-  const [filters, setFilters] = React.useState([])
-  const [oldfilters, setOldFilters] = React.useState([])
+  const [filters, setFilters] = React.useState<FiltersType>([])
+  const [oldfilters, setOldFilters] = React.useState<FiltersType>([])
   const [filterId, setFilterId] = React.useState('')
+  const [currentIdsIndex, setCurrentIdsIndex] = React.useState(-1)
+  const [fetchFilters, setFetchFilters] = React.useState(false)
+
+  function getCurrentId() {
+    return ids[currentIdsIndex]
+  }
+
+  function getPrevId() {
+    if (currentIdsIndex < 0) {
+      return ids[0]
+    }
+    if (currentIdsIndex > 0) {
+      return ids[currentIdsIndex - 1]
+    }
+    // if (currentIdsIndex === 0) {
+    //   return ids[ids.length - 1]
+    // }
+    return ids[currentIdsIndex]
+  }
 
   function onChangeSearch(event: React.ChangeEvent<HTMLInputElement>) {
     const {
       target: { value },
     } = event
+    setCurrentIdsIndex(0)
     setFilterId(value)
-    const idIndex = filters.findIndex((obj) => obj?.field === id)
-    if (idIndex > -1) {
-      if (value === '') {
-        setFilters((oldFilter) => oldFilter.filter((data) => data?.field !== id))
-      } else {
-        const updateId = filters.map((data, i) => {
-          if (i === idIndex) {
-            return { ...data, from_value: `%${value}%` }
-          }
-          return { ...data }
-        })
-        setFilters(updateId)
-      }
-    } else {
-      setFilters([
-        ...filters,
-        {
-          field: id,
-          option: 'CP',
-          from_value: `%${value}%`,
-          data_type: 'S',
-        },
-      ])
-    }
+    setFetchFilters(true)
   }
 
   const searchProps: SearchProps = {
@@ -79,15 +83,50 @@ export default function useFilters(
   }
 
   React.useEffect(() => {
+    if (table.state.data.length === 0) {
+      if (currentIdsIndex < ids.length - 1) {
+        setCurrentIdsIndex((prev) => prev + 1)
+        setFetchFilters(true)
+      } else {
+        setCurrentIdsIndex(0)
+        setFetchFilters(false)
+      }
+    }
+  }, [table.state.data])
+
+  React.useEffect(() => {
     if (app.state.isRequestPrevious) {
       setFilters(body.filters)
+
       setOldFilters(body.filters)
-      setFilterId(body.filters.find((f) => f.field === id)?.from_value.replaceAll('%', '') || '')
+      setFilterId(
+        body.filters.find((f) => f.field === getCurrentId())?.from_value.replaceAll('%', '') || '',
+      )
     } else {
       table.handler.handleFilter(filters)
-      setFilterId(filters.find((f) => f.field === id)?.from_value.replaceAll('%', '') || '')
     }
   }, [app.state.isRequestPrevious, filters])
+
+  React.useEffect(() => {
+    const removedFilterID = filters.filter((f) => !ids.includes(f.field))
+
+    if (fetchFilters) {
+      if (filterId === '') {
+        setFilters(removedFilterID)
+      } else {
+        setFilters([
+          ...removedFilterID,
+          {
+            field: getCurrentId(),
+            option: 'CP',
+            data_type: 'S',
+            from_value: `%${filterId}%`,
+          },
+        ])
+      }
+    }
+    setFetchFilters(false)
+  }, [fetchFilters])
 
   return {
     filters,
