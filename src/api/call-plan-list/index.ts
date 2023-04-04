a3b688e482b62e6f7144bb07e8555fc7721d9490
m_drakbar <daffaraihan03@gmail.com>
2023-04-04 15:02:09 +0700

    build: add api call plan & non call plan

--- src/api/call-plan-list/index.ts
+++ src/api/call-plan-list/index.ts
@@ -1,31 +1,33 @@
 import { call } from 'src/api/BaseApi'
 import { METHODS } from 'src/api/methods'
-// import { CommonListResponse, CommonListParams } from 'src/api/types'
+import { CommonListParams, CommonListResponse } from '../types'
 
-const url = 'v1/callplan-patterns'
+const url = 'v1/callplans'
 const overrideBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL_4
 
-interface CreateCallPlanList {
+interface GenerateCallPlanPayload {
   company_id: string
-  customer_id: string
-  salesman_id: string
-  visit_day: string
-  week_1: string
-  week_2: string
-  week_3: string
-  week_4: string
-  cycle: string
-  sequence: string
-  is_active: string
-  is_default: string
-  visit_day_name: string
+  branch_id: string
+  generate_date: string
 }
 
-export const createCallPlanList = async (params: CreateCallPlanList): Promise<any> => {
+export const getCallPlanList = async (
+  params: CommonListParams,
+): Promise<CommonListResponse<any>> => {
   const response = await call({
     method: METHODS.POST,
     overrideBaseUrl,
     subUrl: `${url}/list`,
+    data: params,
+  })
+  return response.data
+}
+
+export const generateCallPlan = async (params: GenerateCallPlanPayload): Promise<any> => {
+  const response = await call({
+    method: METHODS.POST,
+    overrideBaseUrl,
+    subUrl: `${url}/create`,
     data: params,
   })
   return response.data
---
+++ src/api/non-call-plan-list/index.ts
@@ -0,0 +1,34 @@
+import { call } from 'src/api/BaseApi'
+import { METHODS } from 'src/api/methods'
+import { CommonListParams, CommonListResponse } from '../types'
+
+const url = 'v1/non-callplans'
+const overrideBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL_4
+
+interface GenerateNonCallPlanPayload {
+  company_id: string
+  branch_id: string
+  generate_date: string
+}
+
+export const getNonCallPlanList = async (
+  params: CommonListParams,
+): Promise<CommonListResponse<any>> => {
+  const response = await call({
+    method: METHODS.POST,
+    overrideBaseUrl,
+    subUrl: `${url}/list`,
+    data: params,
+  })
+  return response.data
+}
+
+export const generateNonCallPlan = async (params: GenerateNonCallPlanPayload): Promise<any> => {
+  const response = await call({
+    method: METHODS.POST,
+    overrideBaseUrl,
+    subUrl: `${url}/create`,
+    data: params,
+  })
+  return response.data
+}
