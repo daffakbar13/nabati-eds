8de95babdff5b8f71cdd5f1ba640d5f0c6773ef4
TheDrongs <thedrongs@gmail.com>
2023-04-20 12:50:49 +0700

    FEAT : add 3 new tagColors for callPlanList

--- src/configs/tagColor.ts
+++ src/configs/tagColor.ts
@@ -8,10 +8,11 @@ export const tagColors = {
     'PGI Done',
     'In Stock',
     'Generate',
+    'Visited',
   ],
   geekblue: ['SAP SO Complete'],
-  red: ['Out of Stock', 'Canceled', 'Cancel', 'Rejected', 'Cancelled', 'Undelivered'],
+  red: ['Out of Stock', 'Canceled', 'Cancel', 'Rejected', 'Cancelled', 'Undelivered', 'Unvisited'],
   blue: ['Partial', 'Approved', 'SAP SO Complete'],
   orange: ['Open'],
-  yellow: ['Draft', 'Wait For Approval', 'Pending', 'Reservation'],
+  yellow: ['Draft', 'Wait For Approval', 'Pending', 'Reservation', 'Skip'],
 }
